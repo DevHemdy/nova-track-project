daea7c3a5cba235c20264a47b3965134a27932b3
Leochukwu <leocchukwu@gmail.com>
2025-05-21 00:58:52 +0100

    footer adjusted

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import Swiper from 'swiper';
+
 
 @Component({
   selector: 'app-home',
@@ -9,29 +9,5 @@ import Swiper from 'swiper';
 })
 export class HomeComponent {
 
-
-
-
- swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'vertical',
-  loop: true,
-
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
-});
 
 }
