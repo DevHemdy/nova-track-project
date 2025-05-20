5e955187db4f49e82aa60950993b695a48f0f20b
Hemdy <205894885+DevHemdy@users.noreply.github.com>
2025-05-20 22:23:31 +0100

    features

--- src/app/pages/services/services.component.ts
+++ src/app/pages/services/services.component.ts
@@ -1,11 +1,90 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-services',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './services.component.html',
   styleUrl: './services.component.scss'
 })
 export class ServicesComponent {
+
+
+  advanceTool: any = [
+    {
+      id: 1,
+      icon: "assets/icons/12.png",
+      title: "Advanced Driver Validation System",
+      text: "Ensures vehicle security through biometric verification, including fingerprints and facial recognition, allowing only authorized personnel to operate fleet vehicles."
+    },
+        {
+      id: 1,
+      icon: "assets/icons/12.png",
+      title: "Advanced Driver Validation System",
+      text: "Ensures vehicle security through biometric verification, including fingerprints and facial recognition, allowing only authorized personnel to operate fleet vehicles."
+    },
+        {
+      id: 1,
+      icon: "assets/icons/12.png",
+      title: "Advanced Driver Validation System",
+      text: "Ensures vehicle security through biometric verification, including fingerprints and facial recognition, allowing only authorized personnel to operate fleet vehicles."
+    },
+        {
+      id: 1,
+      icon: "assets/icons/12.png",
+      title: "Advanced Driver Validation System",
+      text: "Ensures vehicle security through biometric verification, including fingerprints and facial recognition, allowing only authorized personnel to operate fleet vehicles."
+    },
+        {
+      id: 1,
+      icon: "assets/icons/12.png",
+      title: "Advanced Driver Validation System",
+      text: "Ensures vehicle security through biometric verification, including fingerprints and facial recognition, allowing only authorized personnel to operate fleet vehicles."
+    },
+        {
+      id: 1,
+      icon: "assets/icons/12.png",
+      title: "Advanced Driver Validation System",
+      text: "Ensures vehicle security through biometric verification, including fingerprints and facial recognition, allowing only authorized personnel to operate fleet vehicles."
+    },
+        {
+      id: 1,
+      icon: "assets/icons/12.png",
+      title: "Advanced Driver Validation System",
+      text: "Ensures vehicle security through biometric verification, including fingerprints and facial recognition, allowing only authorized personnel to operate fleet vehicles."
+    },
+        {
+      id: 1,
+      icon: "assets/icons/12.png",
+      title: "Advanced Driver Validation System",
+      text: "Ensures vehicle security through biometric verification, including fingerprints and facial recognition, allowing only authorized personnel to operate fleet vehicles."
+    },
+        {
+      id: 1,
+      icon: "assets/icons/12.png",
+      title: "Advanced Driver Validation System",
+      text: "Ensures vehicle security through biometric verification, including fingerprints and facial recognition, allowing only authorized personnel to operate fleet vehicles."
+    },
+        {
+      id: 1,
+      icon: "assets/icons/12.png",
+      title: "Advanced Driver Validation System",
+      text: "Ensures vehicle security through biometric verification, including fingerprints and facial recognition, allowing only authorized personnel to operate fleet vehicles."
+    },
+        {
+      id: 1,
+      icon: "assets/icons/12.png",
+      title: "Advanced Driver Validation System",
+      text: "Ensures vehicle security through biometric verification, including fingerprints and facial recognition, allowing only authorized personnel to operate fleet vehicles."
+    },
+        {
+      id: 1,
+      icon: "assets/icons/12.png",
+      title: "Advanced Driver Validation System",
+      text: "Ensures vehicle security through biometric verification, including fingerprints and facial recognition, allowing only authorized personnel to operate fleet vehicles."
+    },
+    
+
+  ]
 
 }
