e85868a21fbe860dfa829c7dd77329855b094b94
Hemdy <205894885+DevHemdy@users.noreply.github.com>
2025-05-20 19:47:41 +0100

    assets

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { ServicesComponent } from './pages/services/services.component';
+import { CompanyComponent } from './pages/company/company.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
 
 export const routes: Routes = [
-    {path: '', component: HomeComponent}
+    {path: '', component: HomeComponent},
+    {path: 'services', component: ServicesComponent},
+    {path: 'company', component: CompanyComponent},
+    {path: 'projects', component: ProjectsComponent},
 ];
