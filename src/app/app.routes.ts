7f85299f602b9bb385a4940c01e50652826f17dc
Hemdy <205894885+DevHemdy@users.noreply.github.com>
2025-05-21 02:21:48 +0100

    ticket page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './pages/home/home.component';
 import { ServicesComponent } from './pages/services/services.component';
 import { CompanyComponent } from './pages/company/company.component';
 import { ProjectsComponent } from './pages/projects/projects.component';
+import { TicketsComponent } from './pages/tickets/tickets.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'services', component: ServicesComponent},
     {path: 'company', component: CompanyComponent},
     {path: 'projects', component: ProjectsComponent},
+    {path: 'tickets', component: TicketsComponent}
 ];
--- src/app/pages/tickets/tickets.component.ts
+++ src/app/pages/tickets/tickets.component.ts
@@ -1,11 +1,41 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-tickets',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './tickets.component.html',
   styleUrl: './tickets.component.scss'
 })
 export class TicketsComponent {
 
+   advanceTool: any = [
+ 
+        {
+      id: 1,
+      icon: "assets/icons/black-1.png",
+      title: "Address",
+      text: "11-12 Ibiyinka Olorunbe Close, off Amodu Ojikutu Street, Lagos, Nigeria"
+    },
+        {
+      id: 1,
+      icon: "assets/icons/black-2.png",
+      title: "Phone",
+      text: "+234 703 444 6666"
+    },
+        {
+      id: 1,
+      icon: "assets/icons/black-3.png",
+      title: "Email",
+      text: "info@novatrack.com"
+    },
+        {
+      id: 1,
+      icon: "assets/icons/black-4.png",
+      title: "Support Hours",
+      text: "Monday – Friday: 8:00 AM – 6:00 PM (WAT)"
+    }
+
+  ]
 }
