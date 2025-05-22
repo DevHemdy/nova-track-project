3d55df1751b2039c01479f16f78980e08e9d626a
Hemdy <205894885+DevHemdy@users.noreply.github.com>
2025-05-22 22:16:59 +0100

    header component
    readjusted and styled properly

--- src/app/reusables/header/header.component.ts
+++ src/app/reusables/header/header.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import {  HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -9,5 +10,11 @@ import { RouterLink } from '@angular/router';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+    isScrolled = false;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.isScrolled = window.pageYOffset > 50;
+  }
 
 }
