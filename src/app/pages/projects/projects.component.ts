7a2d25f43144e7f40d443615111f34b33fd14ff4
Hemdy <205894885+DevHemdy@users.noreply.github.com>
2025-05-27 21:15:01 +0100

    Buttons
    routed all  request consultation buttons

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -1,8 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
